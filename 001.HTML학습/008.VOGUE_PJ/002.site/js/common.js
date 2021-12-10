493cab953627c463e594fd215a6889cf131330b8
Leejw97 <88294755+Leejw97@users.noreply.github.com>
2021-12-10 18:04:04 +0900

    ss

--- 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
@@ -1,48 +1,56 @@
 // 공통기능구현 JS - common.js
 
-//////// 윈도우 스크롤시 이벤트 함수 ///////////
-window.addEventListener("scroll", ()=>{
+///////// 윈도우 스크롤시 이벤트 함수 ////////////
+window.addEventListener("scroll",()=>{
 
     // 스크롤 위치값
     let scTop = this.scrollY;
     // scrollY - 세로축 스크롤 위치값 리턴
     // this는 화살표함수에서 window를 나타냄
 
-    /******************************************* 
-        윈도운 세로 스크롤 위치값을 가져오는 방법
+    /******************************************** 
+        윈도우 세로 스크롤 위치값을 가져오는 방법
         1. this.scrollY
         2. window.scrollY
-        3. ument.scrollingElement.scrollTop
+        3. document.scrollingElement.scrollTop
         4. document.documentElement.scrollTop
-        5. document.querySelector("html").scrollTop;
-    *******************************************/
+        5. document.querySelector("html").scrollTop
+    ********************************************/
 
-    console.log("스크롤중",scTop);
+    // console.log("스크롤중~~~",scTop);
 
-    ///// 스크롤위치값이 100이상일 때 /////
+    // 상단영역
+    let top = document.querySelector("#top");
+    // 상단이동버튼
+    let tbtn = document.querySelector(".tbtn");
+
+    //// 상단영역 스크롤 반응하기 //////////
+    /////// 스크롤위치값이 100이상일때 /////
     if(scTop>=100){ // 100px이상
-        document.querySelector("#top")
-        .classList.add("on")
-    } ///////////////// if /////////////////
+        top.classList.add("on");
+    } /////////// if /////////////
     else{ // 100px 미만
-        document.querySelector("#top")
-        .classList.remove("on")
-    } ////// else ///////
+        top.classList.remove("on");
+    } ///////// else //////////////
 
-}); //////////// scroll 이벤트함수 ///////////
-/////////////////////////////////////////////
 
-// 상단영역요소 전역변수
-let top;
+    //// 상단이동버튼 스크롤 반응하기 //////////
+    /////// 스크롤위치값이 200이상일때 /////
+    if(scTop>=200){ // 200px이상
+        tbtn.classList.add("on");
+    } /////////// if /////////////
+    else{ // 200px 미만
+        tbtn.classList.remove("on");
+    } ///////// else //////////////
+
+}); ////////////// scroll 이벤트함수 ////////////
+////////////////////////////////////////////////
 
 //////////// 로딩구역 /////////////////////////
 window.addEventListener("DOMContentLoaded",
 function(){
 
     console.log("로딩완료!");
-
-    // 전역변수 요소할당
-    top = document.querySelector("#top");
 
     //////////////////////////////////////////
     // 1. 햄버거 버튼 클릭시 //////////////////
@@ -58,7 +66,7 @@ function(){
         // 내부박스
         var mobx = document.querySelector("#mobx");
         // 상단영역 최상위박스
-        var top  = document.querySelector("#top");
+        var top = document.querySelector("#top");
 
         // 3. 변경내용: 
         // (1) 대상의 높이값이 0이면 
@@ -76,20 +84,20 @@ function(){
             // console.log("내부박스높이:",inh);
             tg.style.height = inh + "px";
             tg.style.transition = ".4s ease-out";
-        } //////////// if ///////////////
+        } //////// if ////////////////
         else{ // 높이값이 0이 아니면
             tg.style.height = "0";
         } ///////// else ////////////
 
-        // 추가변경: #top에 클랙스 .mob 추가하여
+        /// 추가변경: #top에 클래스 .mob 추가하여 
         // 높이값 보이는 화면 + y축방향 스크롤
         top.classList.toggle("mob");
-        // classList 객체: 클래스를 넣거나 뺴는 기능객체
-        //  아래 3가지 메소드를 잘 알아두자
-        // (1) add(클래스명) - 클래스 넣기
-        // (2) remove(클래스명) - 클래스 빼기
-        // (3) toggle(클래스명) - 클래스를 넣기/빼기
-        
+        // classList 객체 : 클래스를 넣거나 빼는 기능객체
+        // 아래 3가지 메서드를 잘알아두자!
+        // (1) add(클래스명) - 클래스넣기
+        // (2) remove(클래스명) - 클래스빼기
+        // (3) toggle(클래스명) - 클래스넣기/빼기
+
 
         // a요소는 기본이동 특성이 있어서
         // 이것을 무효화 해주는 코드가 return false임!
@@ -116,10 +124,10 @@ function(){
         if(mosh===0){
             mos.style.height = "50px";
             mos.style.transition = "height .3s ease-out";
-        }
-        else{
+        } //////// if ////////////
+        else {
             mos.style.height = "0";
-        }
+        } //////// else //////////
 
 
 
